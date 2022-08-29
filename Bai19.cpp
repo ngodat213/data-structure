f83449234e34f5b082bfc0c1770507dc1b8ed660
HydraCoder <ngodat.it213@gmail.com>
2022-08-30 02:00:37 +0700

    node struct student

--- Bai19.cpp
+++ Bai19.cpp
@@ -91,14 +91,58 @@ void deleteNode(Node* &pHead){
 	}
 }
 
-void deleteByID(Node* &pHead){
-
-}
-
 void deleteAfter(Node *node){
-    Node* p;
+
 }
+
+void del_id(Node* &pHead, int value){
+    if(!isEmpty(pHead)){
+        Node* tmp = pHead;
+        if(pHead->data.id == value){
+            pHead = pHead->next;
+            delete tmp;
+        }else{
+            Node* p = NULL;
+            while(tmp!=NULL && tmp->data.id!=value){
+                p = tmp;
+                tmp = tmp->next;
+            }
+            if(tmp != NULL){
+                p->next = tmp->next;
+                delete tmp;
+            }
+        }
+    }
+}
+
+void sortNode(Node* &node){
+    Node* i, *j;
+    for(i = node; i != NULL; i = i->next){
+        for(j = i->next; j != NULL; j = j->next){
+            if(i->data.id > j->data.id){
+                int tmp = i->data.id;
+                i->data.id = j->data.id;
+                j->data.id = tmp;
+            }
+        }
+    }
+}
+
 // Function menu
+void menu1(){
+    cout << "1. insert first student" << endl;
+    cout << "2. insert after student" << endl;
+    cout << "3. delete by id student" << endl;
+    cout << "4. delete first student" << endl;
+    cout << "5. delete after student" << endl;
+    cout << "6. show list student" << endl;
+    cout << "7. find node student" << endl;
+    cout << "8. delete all node" << endl;
+    cout << "9. sort node student" << endl;
+    cout << "10. exit" << endl;
+    cout << "[Press the number of your choice]" << endl;
+}
+
 void menu(){
     cout << "1. insert first student" << endl;
     cout << "2. insert after student" << endl;
@@ -106,7 +150,8 @@ void menu(){
     cout << "4. show list student" << endl;
     cout << "5. find node student" << endl;
     cout << "6. delete all node" << endl;
-    cout << "7. exit" << endl;
+    cout << "7. sort node student" << endl;
+    cout << "8. exit" << endl;
     cout << "[Press the number of your choice]" << endl;
 }
 
@@ -130,13 +175,36 @@ int main(){
             insertAfter(pHead, st);
         }
             break;
-        case 3:
+        case 3:{
+            int value;
+            cout << "enter id to delete student: ";
+            cin >> value;
+            del_id(pHead, value);
+        }
             break;
         case 4:
             showList(pHead);
             break;
-        case 7:
-            return false;
+        case 5:{
+            int value;
+            cout << "enter id to find student" << endl;
+            cin >> value;
+            if(findNode(pHead, value) != NULL){
+                outputStudent(findNode(pHead, value)->data);
+            }else{
+                cout << "no info!" << endl;
+            }
+        }
+            break;
+        case 6:{
+            deleteNode(pHead);
+            cout << "delete successful!" << endl;
+        }
+        case 7:{
+            sortNode(pHead);
+            showList(pHead);
+        }
+            break;
         }
     }
     return 0;
