4031787c433e87bdb11a36f387e190716b91c96c
HydraCoder <ngodat.it213@gmail.com>
2022-08-31 19:37:39 +0700

    node student struct bt1

--- Bai23.cpp
+++ Bai23.cpp
@@ -74,8 +74,67 @@ Node* createNode(ST st){
     return node;
 }
 
-void insertFirst(Node* &node, ST st);
+void insertFirst(Node* &node, ST st){
+    Node* p = createNode(st);
+    if(isEmpty(node)){
+        node = p;
+    }else{
+        p->next = node;
+        node = p;
+    }
+}
 
+void showList(Node* node){
+    Node* p = node;
+    while(!isEmpty(p)){
+        outputStudent(p->data);
+        p = p->next;
+    }
+}
+
+ST output_thanScore(Node* node, float value){
+    Node* p = node;
+    while(!isEmpty(p)){
+        if(p->data.score > value){
+            outputStudent(p->data);
+        }
+        p = p->next;
+    }
+}
+
+ST find_name(Node* node, string value){
+    Node* p = node;
+    while(!isEmpty(p)){
+        if(p->data.name == value){
+            return p->data;
+        }
+        p = p->next;
+    }
+    cout << "no info!" << endl;
+}
+
+void sortStudent(Node* &node){
+    Node* i, *j;
+    for(i = node; !isEmpty(node); i=i->next){
+        for(j = i->next; !isEmpty(node); j=j->next){
+            if(i->data.score > j->data.score){
+                ST temp = i->data;
+                i->data = j->data;
+                j->data= temp;
+            }
+        }
+    }
+}
+
+// Main
+void menu(){
+    cout << "insert student" << endl;
+    cout << "output student" << endl;
+    cout << "output student score than \'value\'" << endl;
+    cout << "find student" << endl;
+    cout << "sort student" << endl;
+    cout << "[Press number of your choice]" << endl;
+}
 int main(){
 
 }
