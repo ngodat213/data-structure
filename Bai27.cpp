0e82d0c0377cd1e34cff7eeb97e91808ea93fd5f
HydraCoder <ngodat.it213@gmail.com>
2022-09-12 15:22:09 +0700

    tree

--- Bai27.cpp
+++ Bai27.cpp
@@ -17,10 +17,7 @@ bool isEmpty(Tree* tree){
 }
 
 Tree* createTree(int data){
-    Tree t = new Tree;
-    if(t == NULL){
-        return NULL;
-    }
+    Tree* t = new Node;
     t->data = data;
     t->pLeft = NULL;
     t->pRight = NULL;
@@ -45,15 +42,15 @@ void postOder(Tree* tree){
     cout << tree->data << " ";
 }
 
-void insertFirst(Tree* &tree, int data){
+void insertTree(Tree* &tree, int data){
     Tree* p = createTree(data);
     if(tree == NULL){
         tree = p;
     }else{
-        if(data > tree.data){
-            insertFirst(tree->pRight, data);
+        if(data > tree->data){
+            insertTree(tree->pRight, data);
         }else{
-            insertFirst(tree->pLeft, data)
+            insertTree(tree->pLeft, data);
         }
     }
 }
@@ -69,6 +66,96 @@ Tree* tree_search(Tree* tree, int key){
         tree_search(p->pRight, key);
     }
     return NULL;
+}
+
+void clearTree(Tree* tree){
+    if(tree != NULL){
+        clearTree(tree->pLeft);
+        clearTree(tree->pRight);
+        delete tree;
+    }
+}
+
+int deleteLeft(Tree* p){
+    Tree* q;
+    int x;
+    if(p == NULL){
+        cout << "tree is empty\n";
+
+    }else{
+        q = p->pLeft;
+        x = q->data;
+        if(q == NULL){
+            cout << "tree left is null\n";
+        }else{
+            if(q->pLeft != NULL || q->pRight != NULL){
+                cout << "not is last node\n";
+            }else{
+                q->pLeft = NULL;
+                delete q;
+            }
+        }
+    }
+    return x;
+}
+
+int deleteRight(Tree* p){
+    Tree* q;
+    int x;
+    if(p == NULL){
+        cout << "tree is empty\n";
+    }else{
+        q = p->pRight;
+        x = q->data;
+        if(q == NULL){
+            cout << "tree right is null\n";
+        }else{
+            if(q->pLeft != NULL || q->pRight != NULL){
+                cout << "not is last tree\n";
+            }else{
+                p->pRight = NULL;
+                delete q;
+            }
+        }
+    }
+    return x;
+}
+
+bool removeTree(Tree* tree, int x){
+    if(tree == NULL){
+        return 0;
+    }
+    if(tree->data > x){
+        return removeTree(tree->pLeft, x);
+    }
+    if(tree->data < x){
+        return removeTree(tree->pRight, x);
+    }
+    // tree->data = x
+    Node *t, *f, *rp;
+    t = tree;
+    // TH1, 1 node
+    if(tree->pLeft == NULL){
+        tree = tree->pRight;
+    }else if(tree->pRight == NULL){
+        tree = tree->pRight;
+    }else{
+        f = t;
+        rp = t->pRight; // start from pRight
+        while(rp ->pLeft != NULL){
+            f = rp;
+            rp = rp->pLeft;
+        }
+        t->data = rp->data;
+        if(f == t){
+            f->pRight = rp->pRight;
+        }else{
+            f->pLeft = rp->pRight;
+        }
+        t = rp;
+    }
+    delete t;
+    return 1;
 }
 
 int main(){
